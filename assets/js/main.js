4730243e7ac97e75db252724e373aca0ebd0b7a7
Yaroslav Shevchenko <5y.lmwg@gmail.com>
2018-08-07 12:42:52 -0700

    JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,20 +36,20 @@ var btn = document.getElementById("main-btn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+btn.onclick = function () {
+  modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+span.onclick = function () {
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 function dropMobMenu() {
